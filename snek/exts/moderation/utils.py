a5ecb751c11162d03c3479db9c3b6f12eaccc134
Den4200 <dennis@dennispham.me>
2020-08-03 02:07:58 -0400

    Infractions: Add guild to InfractionPayload

--- snek/exts/moderation/utils.py
+++ snek/exts/moderation/utils.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from datetime import datetime
 import typing as t
 
+import discord
 from discord.ext.commands import Context
 
 from snek.utils import UserObject
@@ -18,7 +19,7 @@ class Infraction(Enum):
     NOTE = auto()
 
 
-InfractionPayloadDict = t.Dict[str, t.Union[str, None, datetime, UserObject, Infraction]]
+InfractionPayloadDict = t.Dict[str, t.Union[str, None, datetime, discord.Guild, UserObject, Infraction]]
 
 
 @dataclass
@@ -28,6 +29,7 @@ class InfractionPayload:
     expires_at: t.Optional[datetime]
     user: UserObject
     actor: UserObject
+    guild: discord.Guild
 
     def to_dict(self) -> InfractionPayloadDict:
         return {
@@ -35,7 +37,8 @@ class InfractionPayload:
             'reason': self.reason,
             'expires_at': self.expires_at.isoformat(),
             'user': self.user.id,
-            'actor': self.actor.id
+            'actor': self.actor.id,
+            'guild': self.guild.id
         }
 
 
