40346e98d0d432ef8f7cb3308a7d39dc0f841e9c
Den4200 <dennis@dennispham.me>
2020-08-15 01:49:19 -0400

    Infractions: Create `send_pardon` method and add `id` field to InfractionPayload

--- snek/exts/moderation/utils.py
+++ snek/exts/moderation/utils.py
@@ -31,16 +31,18 @@ InfractionPayloadDict = t.Dict[str, t.Union[str, None, datetime, discord.Guild, 
 @dataclass
 class InfractionPayload:
     type: Infraction
-    reason: t.Optional[str]
-    expires_at: t.Optional[datetime]
     user: UserObject
     actor: UserObject
     guild: discord.Guild
+    reason: t.Optional[str] = None
+    expires_at: t.Optional[datetime] = None
     active: bool = True,
     hidden: bool = False
+    id: t.Optional[int] = None
 
     def to_dict(self) -> InfractionPayloadDict:
         payload = {
+            'id': self.id,
             'type': self.type.name.lower(),
             'reason': self.reason,
             'user': self.user.id,
@@ -56,23 +58,42 @@ class InfractionPayload:
         return payload
 
     @classmethod
-    def from_dict(cls, ctx: Context, payload: InfractionPayloadDict) -> InfractionPayload:
+    async def from_dict(cls, ctx: Context, payload: InfractionPayloadDict) -> InfractionPayload:
 
-        def get_user(user_id: int) -> UserObject:
-            return ctx.guild.get_member(user_id) or ProxyUser().convert(ctx, user_id)
+        async def get_user(user_id: int) -> UserObject:
+            return ctx.guild.get_member(user_id) or await ProxyUser().convert(ctx, user_id)
 
         self = cls(
-            type=Infraction(payload['type']),
+            id=payload.get('id'),
+            type=Infraction[payload['type'].upper()],
             reason=payload['reason'],
             expires_at=datetime.fromisoformat(payload['expires_at']) if payload['expires_at'] else None,
-            user=get_user(payload['user']),
-            actor=get_user(payload['actor']),
+            user=await get_user(payload['user']),
+            actor=await get_user(payload['actor']),
             guild=ctx.bot.get_guild(payload['guild']),
             active=payload['active'],
             hidden=payload['hidden']
         )
 
         return self
+
+
+def _construct_generic_infr_embed(payload: InfractionPayload, *, infraction: bool) -> discord.Embed:
+    """Constructs a generic embed for infractions and pardons."""
+    infr_type = payload.type.name.capitalize()
+
+    embed = discord.Embed(
+        title=f'{infr_type} {"Infraction" if infraction else "Pardon"}',
+        timestamp=datetime.utcnow(),
+        color=discord.Color.red() if infraction else discord.Color.green()
+    )
+
+    embed.set_author(name=payload.guild.name, icon_url=str(payload.guild.icon_url))
+
+    embed.add_field(name='Guild', value=payload.guild.name)
+    embed.add_field(name='Infraction Type', value=infr_type)
+
+    return embed
 
 
 async def send_infraction(payload: InfractionPayload) -> bool:
@@ -83,18 +104,7 @@ async def send_infraction(payload: InfractionPayload) -> bool:
     True -> Successful
     False -> Unsuccessful
     """
-    infr_type = payload.type.name.capitalize()
-
-    embed = discord.Embed(
-        title=f'{infr_type} Infraction',
-        timestamp=datetime.utcnow(),
-        color=discord.Color.red()
-    )
-
-    embed.set_author(name=payload.guild.name, icon_url=str(payload.guild.icon_url))
-
-    embed.add_field(name='Guild', value=payload.guild.name)
-    embed.add_field(name='Infraction Type', value=infr_type)
+    embed = _construct_generic_infr_embed(payload, infraction=True)
 
     if (expires_at := payload.expires_at) is not None:
         if expires_at == 'permanent':
@@ -108,6 +118,21 @@ async def send_infraction(payload: InfractionPayload) -> bool:
         embed.description = f'**Reason**\n{reason}'
 
     log.trace(f'Sending an infraction notification to user {payload.user.id} from guild {payload.guild.id}.')
+    return await send_private_embed(payload.user, embed)
+
+
+async def send_pardon(payload: InfractionPayload) -> bool:
+    """
+    Notifies about an infraction pardon to a user via DMs and returns
+    a boolean showing whether or not the DM was successful.
+
+    True -> Successful
+    False -> Unsuccessful
+    """
+    embed = _construct_generic_infr_embed(payload, infraction=False)
+    embed.description = f'Your {payload.type.lower()} has been pardoned.'
+
+    log.trace(f'Sending an infraction pardon notification to user {payload.user.id} from guild {payload.guild.id}.')
     return await send_private_embed(payload.user, embed)
 
 
