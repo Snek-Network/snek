b5126f9424e6b62e24d3855f46fb09c7ace79a09
Xithrius <xithrius@gmail.com>
2020-08-19 15:41:29 -0700

    Defaulting to `'` instead of `"` for strings, changed typing importing.
    Also loaded in MAGICBALL constant.

--- snek/exts/randomization.py
+++ snek/exts/randomization.py
@@ -1,23 +1,30 @@
+import json
+import typing as t
+from pathlib import Path
 from random import choice, randint, random
-from typing import Callable, Tuple, Union
 
 from discord import Embed, Message
-from discord.ext.commands import BadArgument, Cog, Context, MessageConverter, command
+from discord.ext.commands import BadArgument, MessageConverter, Cog, Context, command
 
 from snek.bot import Snek
 
+
 UWU = {
-    "fi": "fwi",
-    "l": "w",
-    "r": "w",
-    "some": "sum",
-    "th": "d",
-    "thing": "fing",
-    "tho": "fo",
+    'fi': 'fwi',
+    'l': 'w',
+    'r': 'w',
+    'some': 'sum',
+    'th': 'd',
+    'thing': 'fing',
+    'tho': 'fo',
     "you're": "yuw'we",
-    "your": "yur",
-    "you": "yuw",
+    'your': 'yur',
+    'you': 'yuw',
 }
+
+
+with Path('snek/resources/magic8ball.json').open('r', encoding='utf8') as f:
+    MAGICBALL = json.load(f)
 
 
 class Randomization(Cog):
@@ -52,7 +59,7 @@ class Randomization(Cog):
 
         # Don't put >>> if only embed present
         if converted_text:
-            converted_text = f">>> {converted_text.lstrip('> ')}"
+            converted_text = f'>>> {converted_text.lstrip("> ")}'
 
         await ctx.send(content=converted_text, embed=embed)
 
@@ -71,7 +78,7 @@ class Randomization(Cog):
 
         # Don't put >>> if only embed present
         if converted_text:
-            converted_text = f">>> {converted_text.lstrip('> ')}"
+            converted_text = f'>>> {converted_text.lstrip("> ")}'
 
         await ctx.send(content=converted_text, embed=embed)
 
@@ -104,7 +111,7 @@ class Randomization(Cog):
         await ctx.send(f'`{key}`: {choice(MAGICBALL[key])}')
 
     @staticmethod
-    async def _get_text_and_embed(ctx: Context, text: str) -> Tuple[str, Union[Embed, None]]:
+    async def _get_text_and_embed(ctx: Context, text: str) -> t.Tuple[str, t.Union[Embed, None]]:
         """Gets the text and embed from a possible link to a discord Message object."""
         embed = None
         message = await Randomization._get_discord_message(ctx, text)
@@ -119,7 +126,7 @@ class Randomization(Cog):
         return text, embed
 
     @staticmethod
-    async def _get_discord_message(ctx: Context, text: str) -> Union[Message, str]:
+    async def _get_discord_message(ctx: Context, text: str) -> t.Union[Message, str]:
         """Converts a message ID, link, or message to a message object or a string."""
         try:
             text = await MessageConverter().convert(ctx, text)
@@ -131,20 +138,20 @@ class Randomization(Cog):
             return text
 
     @staticmethod
-    def _convert_embed(func: Callable, embed: Embed) -> Embed:
+    def _convert_embed(func: t.Callable, embed: Embed) -> Embed:
         """Converts the title, description, footer, and field text with the given function"""
         embed_dict = embed.to_dict()
 
-        embed_dict["title"] = func(embed_dict.get("title", ""))
-        embed_dict["description"] = func(embed_dict.get("description", ""))
+        embed_dict['title'] = func(embed_dict.get('title', ''))
+        embed_dict['description'] = func(embed_dict.get('description', ''))
 
-        if "footer" in embed_dict:
-            embed_dict["footer"]["text"] = func(embed_dict["footer"].get("text", ""))
+        if 'footer' in embed_dict:
+            embed_dict['footer']['text'] = func(embed_dict['footer'].get('text', ''))
 
-        if "fields" in embed_dict:
-            for field in embed_dict["fields"]:
-                field["name"] = func(field.get("name", ""))
-                field["value"] = func(field.get("value", ""))
+        if 'fields' in embed_dict:
+            for field in embed_dict['fields']:
+                field['name'] = func(field.get('name', ''))
+                field['value'] = func(field.get('value', ''))
 
         return Embed.from_dict(embed_dict)
 
