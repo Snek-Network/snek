d52e01813a96ef9c36ad81f823e31b00345ad017
Xithrius <xithrius@gmail.com>
2020-08-18 23:49:00 -0700

    Moved randomization extension out of own folder.

--- snek/exts/fun/__init__.py
+++
@@ -1,7 +0,0 @@
-from snek.bot import Snek
-from snek.exts.fun.randomization import Randomization
-
-
-def setup(bot: Snek) -> None:
-    """Load the `Randomization` cog."""
-    bot.add_cog(Randomization(bot))
--- snek/exts/randomization.py
+++ snek/exts/randomization.py
@@ -157,3 +157,8 @@ class Randomization(Cog):
                 field["value"] = func(field.get("value", ""))
 
         return Embed.from_dict(embed_dict)
+
+
+def setup(bot: Snek) -> None:
+    """Load the `Randomization` cog."""
+    bot.add_cog(Randomization(bot))
